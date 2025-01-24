d88c1b3913a09eec74198e31db42f9bced6c7688
DanielCasco <119058961+DanielCasco@users.noreply.github.com>
2025-01-24 15:54:51 +0000

    Update Manutencao.java

--- Trabalho MDS/TheHotel/src/main/java/com/thehotel/Manutencao.java
+++ Trabalho MDS/TheHotel/src/main/java/com/thehotel/Manutencao.java
@@ -21,5 +21,45 @@ public class Manutencao {
         this.realizada = true;
     }
 
-    
+    // Getters e setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
 }
+
